f775fef1192c3ca95da73755a851ea8af4e1d2b7
Taly Guryn <vitalik7tv@yandex.ru>
2017-07-24 18:40:59 +0300

    Initial commit

---
+++ deploy.sample.py
@@ -0,0 +1,21 @@
+"""
+Here is a sample script which uses DeployServer
+
+Run and test your script:
+$ python3 myProject_autodeploy.py
+
+To run script with Screen silently:
+$ screen -dmS myProject_autodeploy python3 myProject_autodeploy.py
+
+CodeX.Deploy repo: https://github.com/codex-team/deploy
+Screen docs: https://www.gnu.org/software/screen/manual/screen.html
+"""
+
+from DeployServer import DeployServer
+
+
+DeployServer({
+    'server_address': 'http://mydomain.com',
+    'port': 1234,
+    'deploy': 'cd /var/www/myProject; git pull;'
+})
