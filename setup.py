64087e1c9c1cfa065726a8a7b0c622a2457f11a8
N0str <ntpcp@yandex.ru>
2018-02-04 18:44:13 +0300

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='deployserver',
-    version='0.2.0',
+    version='0.3.0',
     packages = find_packages(),
     description='Deploy your project automatically when git branch was updated.',
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
