0c98d291f4b6a129f226e1fdb1fec6031d0cae93
gohabereg <gohabereg@gmail.com>
2018-08-08 19:18:21 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='deployserver',
-    version='0.3.1',
+    version='0.4.0',
     packages = find_packages(),
     description='Deploy your project automatically when git branch was updated.',
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
